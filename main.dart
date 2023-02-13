95afc5c031009a26756e50a738a31d038f459f8b
unknown <danasheer21111@gmail.com>
2023-02-13 18:32:51 +0300

    done

--- main.dart
+++ main.dart
@@ -1,7 +1,20 @@
+
 void main(){
-  // Write your code here
-  
-  
-  
-  
+  const num = [5,3,15,4,1];
+  int sum =0;
+  num.forEach((number) => sum += number );
+  print (sum); 
+  const ages = [21, 27,18,19];
+  final allowed = ages.where((age)=> age>=21 );
+  print (allowed);
+  List months = [28,22,24,26];
+  findOdds(months);
+}
+
+void findOdds(List months){
+
+final odd = months.firstWhere((month) => month%2!=0 , orElse: ()=> "you are not lucky" );
+print(odd);
+
+
 }
